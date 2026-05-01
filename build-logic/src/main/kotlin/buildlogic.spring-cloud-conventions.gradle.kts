276e7452e657d61b55e2fdff0d3b32a8e084a6af
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-01 12:08:09 +0800

    feat: 添加 Spring Cloud 和 Consul 依赖配置

---
+++ build-logic/src/main/kotlin/buildlogic.spring-cloud-conventions.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("buildlogic.java-common-conventions")
+}
+
+dependencies {
+    implementation(platform(libs.spring.boot.dependencies))
+    implementation(platform(libs.spring.cloud.dependencies))
+    
+    implementation(libs.spring.cloud.starter.consul.discovery)
+    implementation(libs.spring.cloud.starter.consul.config)
+}
