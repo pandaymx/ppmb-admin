129bb5d6a7e1fddf0b7e7b9a57390adad5098ae2
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-01 17:16:17 +0800

    feat(gateway): 初始化 ppmb-gateway 子项目并配置路由

---
+++ ppmb-gateway/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("buildlogic.spring-cloud-conventions")
+    alias(libs.plugins.spring.boot)
+}
+
+dependencies {
+    implementation(libs.spring.cloud.starter.gateway.server.webmvc)
+}
