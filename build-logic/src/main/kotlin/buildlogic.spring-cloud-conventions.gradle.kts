4cc2b17a1e16dcbf9058eb10a7f15bc147de2769
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-01 12:08:39 +0800

    feat(deps): 为 Spring Cloud Consul 增加负载均衡依赖

--- build-logic/src/main/kotlin/buildlogic.spring-cloud-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.spring-cloud-conventions.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 dependencies {
     implementation(platform(libs.spring.boot.dependencies))
     implementation(platform(libs.spring.cloud.dependencies))
-    
+
     implementation(libs.spring.cloud.starter.consul.discovery)
     implementation(libs.spring.cloud.starter.consul.config)
+    implementation(libs.spring.cloud.starter.loadbalancer)
 }
