c89b7b54f6e756a37790bee9eeb10a1543211e92
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-03 20:59:12 +0800

    refactor(common-redis): 修复 Redis 序列化弃用警告并增强 Jackson 安全性

--- ppmb-common/ppmb-common-redis/src/main/java/top/ppmblszdp/common/redis/util/TwoLevelCacheMessage.java
+++ ppmb-common/ppmb-common-redis/src/main/java/top/ppmblszdp/common/redis/util/TwoLevelCacheMessage.java
@@ -1,6 +1,5 @@
 package top.ppmblszdp.common.redis.util;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class TwoLevelCacheMessage {
   private String cacheName;
   private String key;
