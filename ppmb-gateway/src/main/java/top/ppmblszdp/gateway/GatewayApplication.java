ca39d90cac104ab7c07e4eade2e0ac9757e78caa
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-03 23:50:10 +0800

    refactor(gateway): 移除 Feign 客户端支持

--- ppmb-gateway/src/main/java/top/ppmblszdp/gateway/GatewayApplication.java
+++ ppmb-gateway/src/main/java/top/ppmblszdp/gateway/GatewayApplication.java
@@ -2,11 +2,15 @@ package top.ppmblszdp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 网关服务启动类.
+ *
+ * <p>Gateway 的职责是路由转发，不应该作为 Feign 客户端调用其他服务。 因此不启用 @EnableFeignClients。
+ */
 @SpringBootApplication
-@EnableFeignClients(basePackages = "top.ppmblszdp")
 public class GatewayApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
   }
