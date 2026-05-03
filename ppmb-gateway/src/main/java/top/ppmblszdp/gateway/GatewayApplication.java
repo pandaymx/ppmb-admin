3b59107f0f6876a85945eac255591ab41540d62e
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-03 20:21:25 +0800

    feat(system, gateway): 在应用中开启 FeignClients 支持

--- ppmb-gateway/src/main/java/top/ppmblszdp/gateway/GatewayApplication.java
+++ ppmb-gateway/src/main/java/top/ppmblszdp/gateway/GatewayApplication.java
@@ -2,10 +2,10 @@ package top.ppmblszdp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "top.ppmblszdp")
 public class GatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
