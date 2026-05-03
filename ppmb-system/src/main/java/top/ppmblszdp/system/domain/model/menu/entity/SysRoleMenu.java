e28eaf112fb063fc93a62d7daa8f3275e29665ef
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-03 14:02:37 +0800

    refactor(system): 修复 SysMenu 和 SysRoleMenu 缺失 equals/hashCode 导致的 Sonar S2160 问题

--- ppmb-system/src/main/java/top/ppmblszdp/system/domain/model/menu/entity/SysRoleMenu.java
+++ ppmb-system/src/main/java/top/ppmblszdp/system/domain/model/menu/entity/SysRoleMenu.java
@@ -3,12 +3,14 @@ package top.ppmblszdp.system.domain.model.menu.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import top.ppmblszdp.common.domain.entity.BaseChildEntity;
 
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "sys_role_menu")
 public class SysRoleMenu extends BaseChildEntity {
