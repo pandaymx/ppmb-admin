7a55d96816957bbb2dca2bfdfe49fc3338003d68
皮皮萌宝 <panda1943575780@outlook.com>
2026-05-10 14:02:52 +0800

    fix(common-mq): 移除冗余的 @EnableJpaRepositories 以解决 Bean 定义冲突

--- ppmb-common/ppmb-common-mq/src/main/java/top/ppmblszdp/common/mq/ReliableMqConfig.java
+++ ppmb-common/ppmb-common-mq/src/main/java/top/ppmblszdp/common/mq/ReliableMqConfig.java
@@ -5,12 +5,10 @@ import java.util.concurrent.Executors;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @AutoConfiguration
 @EnableScheduling
-@EnableJpaRepositories(basePackages = "top.ppmblszdp.common.mq.repository")
 @ComponentScan(basePackages = {"top.ppmblszdp.common.mq.service", "top.ppmblszdp.common.mq.job"})
 public class ReliableMqConfig {
 
